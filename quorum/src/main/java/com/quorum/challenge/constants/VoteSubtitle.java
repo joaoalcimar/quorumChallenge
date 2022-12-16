fb7914e3bb0a5d933d0c8646b3e802c3ea4d8725
João <joao.alcimar.junior@gmail.com>
2022-12-16 10:13:24 -0300

    Create VoteSubtitle Constant File

---
+++ quorum/src/main/java/com/quorum/challenge/constants/VoteSubtitle.java
@@ -0,0 +1,7 @@
+package com.quorum.challenge.constants;
+
+public class VoteSubtitle {
+
+    public static final Integer FAVORABLE_VOTE = 1;
+    public static final Integer UNFAVORABLE_VOTE = 2;
+}
